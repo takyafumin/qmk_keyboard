b33c3e60f1e3807aaa708d3c832892af5066e0cd
Takafumi Fukai <takyafumin@gmail.com>
2024-10-14 13:41:56 +0900

    wing42c, キーマップ最新化

--- keyboards/wings42/rev2/keymaps/takyafumin/keymap.c
+++ keyboards/wings42/rev2/keymaps/takyafumin/keymap.c
@@ -25,7 +25,8 @@ enum layer_names {
 enum custom_keycodes {
     MAC_PRSC = SAFE_RANGE,
     L1_EN,
-    L2_JP
+    L2_JP,
+    HOTKEY 
 };
 
 // --------------------
@@ -53,8 +54,8 @@ enum custom_keycodes {
 #define ALT_D    RALT_T(KC_D)
 #define GUI_S    LGUI_T(KC_S)
 #define SFT_Z    LSFT_T(KC_Z)
-#define ALTQUOT  RALT_T(KC_QUOT)
-#define GUILBRC  RGUI_T(KC_LBRC)
+#define GUIQUIT  RGUI_T(KC_QUOT)
+#define ALTLBRC  RALT_T(KC_LBRC)
 // right home mods key
 #define CTLSCN   RCTL_T(KC_SCLN)
 #define ALT_K    RALT_T(KC_K)
@@ -74,49 +75,49 @@ enum custom_keycodes {
 #define L1_J     LT(_LEFT , KC_J)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE] = LAYOUT_split_3x6_3(
+    [_BASE] = LAYOUT_split_3x6_3_2(
     //,--------+--------+--------+--------+--------+--------.                     ,--------+---------+--------+---------+--------+--------.
         XXXXXXX, KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                       KC_Y   , KC_U    , KC_I   , KC_O    , KC_P   , XXXXXXX,
     //|--------+--------+--------+--------+--------+--------|                     |--------+---------+--------+---------+--------+--------|
         XXXXXXX, CTL_A  , GUI_S  , ALT_D  , L1_F   , KC_G   ,                       KC_H   , L1_J    , ALT_K  , GUI_L   , CTLSCN , XXXXXXX,
     //|--------+--------+--------+--------+--------+--------|                     |--------+---------+--------+---------+--------+--------|
-        XXXXXXX, SFT_Z  , KC_X   , KC_C   , KC_V   , KC_B   ,                       KC_N   , KC_M    , KC_COMM, KC_DOT  , KC_SLSH, XXXXXXX,
+        XXXXXXX, SFT_Z  , KC_X   , KC_C   , KC_V   , KC_B   ,                       KC_N   , KC_M    , KC_COMM, KC_DOT  , SFTSLS , XXXXXXX,
     //`--------+--------+--------+--------+--------+--------/                     \--------+---------+--------+---------+--------+--------'
                                    GUIESC , L1_EN  , S_SPC  ,                       S_ENT  , L2_JP   , GUITAB 
     //                          `+--------+--------+--------'                     `--------+---------+--------+'
     ),
 
-    [_LEFT] = LAYOUT_split_3x6_3(
+    [_LEFT] = LAYOUT_split_3x6_3_2(
     //,--------+--------+--------+--------+--------+--------.                     ,--------+--------+--------+--------+--------+--------.
         _______, KC_EXLM, KC_AT  , KC_HASH, KC_DLR , KC_PERC,                       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, QK_RBT ,
     //|--------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------|
-        _______, CTLTAB , ALTQUOT, GUILBRC, KC_RBRC, KC_BSLS,                       KC_LEFT, KC_DOWN, KC_UP  ,KC_RIGHT, KC_COLN, EE_CLR ,
+        _______, CTLTAB , GUIQUIT, ALTLBRC, KC_RBRC, KC_BSLS,                       KC_LEFT, KC_DOWN, KC_UP  ,KC_RIGHT, KC_COLN, EE_CLR ,
     //|--------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------|
         _______, KC_LSFT, KC_DQT , KC_LCBR, KC_RCBR, KC_PIPE,                       KC_EQL , KC_MINS, KC_LT  , KC_GT  , KC_QUES, QK_BOOT,
     //`--------+--------+--------+--------+--------+--------/                     \--------+--------+--------+--------+--------+--------'
-                                  _______, _______, KC_LSFT ,                        S_BS   , EXTENT , DEL
+                                  _______, _______,  _______,                        S_BS   , EXTENT , DEL
     //                          `+--------+--------+--------'                      `--------+-------+--------+'
     ),
 
-    [_RIGHT] = LAYOUT_split_3x6_3(
+    [_RIGHT] = LAYOUT_split_3x6_3_2(
     //,--------+--------+--------+--------+--------+--------.                     ,--------+--------+--------+--------+--------+--------.
         _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                       KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______,
     //|--------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------|
-        _______, KC_GRV , KC_QUOT, KC_LBRC, KC_RBRC, KC_BSLS,                       XXXXXXX, XXXXXXX, KC_RGUI, KC_RALT, CTLQUOT, _______,
+        _______, KC_GRV , KC_QUOT, KC_LBRC, KC_RBRC, KC_BSLS,                       XXXXXXX, XXXXXXX, KC_RALT, KC_RGUI, CTLQUOT, _______,
     //|--------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------|
         _______, KC_TILD, KC_DQT , KC_LCBR, KC_RCBR, KC_PIPE,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RSFT, _______,
     //`--------+--------+--------+--------+--------+--------/                     \--------+--------+--------+--------+--------+--------'
-                                  _______ , EXT_0 ,  _______,                       _______, _______, _______
+                                  _______ , EXT_0 ,  KC_LSFT,                       _______, _______, _______
     //                          `+--------+--------+--------'                     `--------+--------+--------+'
     ),
 
-    [_EXTRA] = LAYOUT_split_3x6_3(
+    [_EXTRA] = LAYOUT_split_3x6_3_2(
     //,--------+--------+--------+--------+--------+--------.                     ,--------+--------+--------+--------+--------+--------.
         _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                       KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , _______,
     //|--------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------|
         _______,MAC_PRSC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_HOME, KC_PGDN, KC_PGUP, KC_END , KC_PSCR, _______,
     //|--------+--------+--------+--------+--------+--------|                     |--------+--------+--------+--------+--------+--------|
-        _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_F11 , KC_F12 , XXXXXXX, XXXXXXX, KC_MENU, _______,
+        _______, KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                       KC_F11 , KC_F12 , HOTKEY , XXXXXXX, KC_MENU, _______,
     //`--------+--------+--------+--------+--------+--------/                     \--------+--------+--------+--------+--------+--------'
                                   _______, _______, _______,                        _______, _______, _______
     //                          `+--------+--------+--------'                     `--------+--------+--------+'
@@ -150,54 +151,61 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case L1_EN:
             if (record->event.pressed) {
                 layer_on(_LEFT);
-                // キーが押されたとき
-                if (l2_state.tap_count > 0) {
-                    // 連続タップ後のホールドが開始された
-                    l2_state.is_tap_hold = true;
-                } else {
-                    // シングルタップのためのタイマーを開始
-                    l2_state.tap_timer = timer_read();
-                }
+                l2_state.tap_timer = timer_read();
+                // if (l2_state.tap_count > 0) {
+                //     // 連続タップ後のホールドが開始された
+                //     l2_state.is_tap_hold = true;
+                // } else {
+                //     // シングルタップのためのタイマーを開始
+                //     l2_state.tap_timer = timer_read();
+                // }
             } else {
                 // キーが離されたとき
                 layer_off(_LEFT);
-                if (l2_state.is_tap_hold) {
-                    l2_state.is_tap_hold = false;
-                } else if (timer_elapsed(l2_state.tap_timer) < TAPPING_TERM) {
+                if (timer_elapsed(l2_state.tap_timer) < TAPPING_TERM) {
                     tap_code(EN);
-                    l2_state.tap_count++;
-                    wait_ms(200); // 200ms以内に連続タップがなければタップカウントをリセット
-                    l2_state.tap_count = 0;
-                } else {
-                    l2_state.is_tap_hold = true;
                 }
+                // if (l2_state.is_tap_hold) {
+                //     l2_state.is_tap_hold = false;
+                // } else if (timer_elapsed(l2_state.tap_timer) < TAPPING_TERM) {
+                //     tap_code(EN);
+                //     l2_state.tap_count++;
+                //     wait_ms(50); // 200ms以内に連続タップがなければタップカウントをリセット
+                //     l2_state.tap_count = 0;
+                // } else {
+                //     l2_state.is_tap_hold = true;
+                // }
             }
             return false; // このキーのデフォルトの動作を無効化
 
         case L2_JP:
             if (record->event.pressed) {
                 layer_on(_RIGHT);
+                l1_state.tap_timer = timer_read();
                 // キーが押されたとき
-                if (l1_state.tap_count > 0) {
-                    // 連続タップ後のホールドが開始された
-                    l1_state.is_tap_hold = true;
-                } else {
-                    // シングルタップのためのタイマーを開始
-                    l1_state.tap_timer = timer_read();
-                }
+                // if (l1_state.tap_count > 0) {
+                //     // 連続タップ後のホールドが開始された
+                //     l1_state.is_tap_hold = true;
+                // } else {
+                //     // シングルタップのためのタイマーを開始
+                //     l1_state.tap_timer = timer_read();
+                // }
             } else {
                 // キーが離されたとき
                 layer_off(_RIGHT);
-                if (l1_state.is_tap_hold) {
-                    l1_state.is_tap_hold = false;
-                } else if (timer_elapsed(l1_state.tap_timer) < TAPPING_TERM) {
+                if (timer_elapsed(l1_state.tap_timer) < TAPPING_TERM) {
                     tap_code(JP);
-                    l1_state.tap_count++;
-                    wait_ms(200); // 200ms以内に連続タップがなければタップカウントをリセット
-                    l1_state.tap_count = 0;
-                } else {
-                    l1_state.is_tap_hold = true;
                 }
+                // if (l1_state.is_tap_hold) {
+                //     l1_state.is_tap_hold = false;
+                // } else if (timer_elapsed(l1_state.tap_timer) < TAPPING_TERM) {
+                //     tap_code(JP);
+                //     l1_state.tap_count++;
+                //     wait_ms(50); // 200ms以内に連続タップがなければタップカウントをリセット
+                //     l1_state.tap_count = 0;
+                // } else {
+                //     l1_state.is_tap_hold = true;
+                // }
             }
             return false; // このキーのデフォルトの動作を無効化
 
@@ -233,6 +241,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 register_code(KC_LGUI);
                 tap_code(KC_4);
                 unregister_code(KC_LGUI);
+                unregister_code(KC_LSFT);
+            }
+            return false;
+            break;
+
+        // HOTKEY
+        case HOTKEY:
+            if (record->event.pressed) {
+                register_code(KC_LSFT);
+                tap_code(KC_LGUI);
                 unregister_code(KC_LSFT);
             }
             return false;
